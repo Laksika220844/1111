7f37f7113c6c4cffcc285925bdd985ce290467e4
Laksika220844 <157354791+Laksika220844@users.noreply.github.com>
2024-01-23 10:05:09 +0700

    Add files via upload

---
+++ WebApplication1/Models/Home.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class Home
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Usename { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
